7aeb0221e572421d65390dc6ebb4fe69b6da19b9
donitasdimbo <2233034021@correo.ler.uam.mx>
2025-12-02 09:32:38 -0600

    imagen del catalogo, orden de carpetas, nuevas rutas ordenadas

--- script.js
+++ script.js
@@ -8,3 +8,36 @@ function openPanel(element) {
 function closePanel() {
     document.getElementById("panel").classList.remove("open");
 }
+
+//para la imagen en superficie
+let activeImg = null;
+
+function openPanel(element) {
+
+    const texto = element.querySelector(".info").innerHTML;
+    const imagen = element.querySelector("img");
+
+    document.getElementById("panelText").innerHTML = texto;
+    document.getElementById("panel").classList.add("open");
+
+    document.getElementById("overlay").classList.add("active");
+
+    // quitar clase anterior si había
+    if (activeImg) {
+        activeImg.classList.remove("active-image");
+    }
+
+    // mover imagen
+    imagen.classList.add("active-image");
+    activeImg = imagen;
+}
+
+function closePanel() {
+    document.getElementById("panel").classList.remove("open");
+    document.getElementById("overlay").classList.remove("active");
+
+    if (activeImg) {
+        activeImg.classList.remove("active-image");
+        activeImg = null;
+    }
+}
